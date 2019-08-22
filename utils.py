35ae6551219bf4e1c604da5419677f15d7f3c315
Negin Shirvani <negi_n.shirvani@yahoo.com>
2019-08-22 15:17:57 +0430

    utils.py

--- utils.py
+++ utils.py
@@ -3,7 +3,13 @@ import json
 
 SEMESTER = "13981"
 users = []
-sess_data = {}
+sess_data = []
+
+def get_dep_by_id(dep_id):
+    for dep in sess_data:
+        if dep['id'] == dep_id:
+            return dep
+
 
 class User:
     def __init__(self, chat_id):
@@ -11,9 +17,15 @@ class User:
         self.dep_id = '2903' 
         self.courses = []
     
-    def search_course(self):
-        pass
+    def search_course(self, qs):
+        result = []
+        for course in get_dep_by_id(self.dep_id)['courses']:
+            if qs in course['title']:
+                result.append(course)
+        return result
+
         
+
     # def __eq__(self, num):
     #     return self.chat_id == num
 
@@ -25,6 +37,19 @@ def get_user(chat_id):
     u = User(chat_id)
     users.append(u)
     return u
+
+def search_department(qs):
+    result = []
+    for department in sess_data:
+            if qs in department['title']:
+                result.append(department)
+    return result
+
+def course_to_str(course):
+    qstr = ''
+    qstr = course['title'] + 'گروه' + course['group'] + '\n' +course['time_room'] + '\n' + course['teacher']
+    return qstr
+
 
 def load_data():
     global sess_data
