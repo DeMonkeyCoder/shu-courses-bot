7ba9c48f56ef2ce5b22a7c9c6ac8936ebbfeec1b
Hassan <hassan.abbp@gmail.com>
2019-08-24 08:49:12 +0430

    Refactor the code

--- bot.py
+++ bot.py
@@ -10,92 +10,96 @@ from telepot.namedtuple import ReplyKeyboardMarkup, KeyboardButton
 from config import TOKEN, PROXY
 from utils import course_to_str, course_to_str, get_user, MenuState, search_department
 
+
 def send_welcome_message(chat_id):
     bot.sendMessage(chat_id,
-        'لطفا از منوی بات گزینه مورد نظرتان را انتخاب کنید',
-        'Markdown',
-        reply_markup=ReplyKeyboardMarkup(
-            keyboard=[
-                [
-                    KeyboardButton(text="انتخاب بخش")
-                ],
-                [
-                    KeyboardButton(text="جستجوی درس های بخش")
-                ],
-                [
-                    KeyboardButton(text="درس های انتخاب شده")
-                ],
-            ]
-        ),
-    )
+                    'لطفا از منوی بات گزینه مورد نظرتان را انتخاب کنید',
+                    'Markdown',
+                    reply_markup=ReplyKeyboardMarkup(
+                        keyboard=[
+                            [
+                                KeyboardButton(text="انتخاب بخش")
+                            ],
+                            [
+                                KeyboardButton(text="جستجوی درس های بخش")
+                            ],
+                            [
+                                KeyboardButton(text="درس های انتخاب شده")
+                            ],
+                        ]
+                    ),
+                    )
+
 
 def send_select_department_message(chat_id):
     bot.sendMessage(chat_id,
-        'لطفا تمام یا قسمتی از نام بخش مورد نظر را وارد کنید',
-        'Markdown',
-        reply_markup=ReplyKeyboardMarkup(
-            keyboard=[
-                [
-                    KeyboardButton(text="بازگشت")
-                ],
-            ]
-        ),
-    )
+                    'لطفا تمام یا قسمتی از نام بخش مورد نظر را وارد کنید',
+                    'Markdown',
+                    reply_markup=ReplyKeyboardMarkup(
+                        keyboard=[
+                            [
+                                KeyboardButton(text="بازگشت")
+                            ],
+                        ]
+                    ),
+                    )
+
 
 def send_search_course_message(chat_id):
     bot.sendMessage(chat_id,
-        'لطفا تمام یا قسمتی از نام درس مورد نظر را وارد کنید',
-        'Markdown',
-        reply_markup=ReplyKeyboardMarkup(
-            keyboard=[
-                [
-                    KeyboardButton(text="بازگشت")
-                ],
-            ]
-        ),
-    )
+                    'لطفا تمام یا قسمتی از نام درس مورد نظر را وارد کنید',
+                    'Markdown',
+                    reply_markup=ReplyKeyboardMarkup(
+                        keyboard=[
+                            [
+                                KeyboardButton(text="بازگشت")
+                            ],
+                        ]
+                    ),
+                    )
+
 
 def send_not_found_message(chat_id):
     bot.sendMessage(chat_id,
-        'موردی یافت نشد',
-        'Markdown',
-        reply_markup=ReplyKeyboardMarkup(
-            keyboard=[
-                [
-                    KeyboardButton(text="بازگشت")
-                ],
-            ]
-        ),
-    )
+                    'موردی یافت نشد',
+                    'Markdown',
+                    reply_markup=ReplyKeyboardMarkup(
+                        keyboard=[
+                            [
+                                KeyboardButton(text="بازگشت")
+                            ],
+                        ]
+                    ),
+                    )
+
 
 def handle(msg):
-    
     content_type, chat_type, chat_id = telepot.glance(msg)
     if chat_type == u'private' and content_type == 'text':
         user = get_user(chat_id)
         # print(get_user(chat_id).search_course(msg['text']))
         # print([course_to_str(c) for c in get_user(chat_id).search_course(msg['text'])])
-        if(user.menu_state == MenuState.GENERAL):
-            if(msg['text'] == 'انتخاب بخش'):
+        if user.menu_state == MenuState.GENERAL:
+            if msg['text'] == 'انتخاب بخش':
                 user.menu_state = MenuState.SELECT_DEPARTMENT
                 send_select_department_message(chat_id)
-            elif(msg['text'] == 'جستجوی درس های بخش'):
+            elif msg['text'] == 'جستجوی درس های بخش':
                 user.menu_state = MenuState.SEARCH_COURSE
                 send_search_course_message(chat_id)
             else:
                 send_welcome_message(chat_id)
 
-        elif(user.menu_state == MenuState.SELECT_DEPARTMENT):
+        elif user.menu_state == MenuState.SELECT_DEPARTMENT:
 
-            if(msg['text'] == 'بازگشت'):
+            if msg['text'] == 'بازگشت':
                 user.menu_state = MenuState.GENERAL
                 send_welcome_message(chat_id)
 
-            elif(msg['text'][0:4] == '/dep'):
+            elif msg['text'][0:4] == '/dep':
                 user.dep_id = msg['text'][4:]
                 bot.sendMessage(chat_id,
-                    'با موفقیت ثبت شد'
-                )
+                                'با موفقیت ثبت شد'
+                                )
                 user.menu_state = MenuState.GENERAL
                 send_welcome_message(chat_id)
 
@@ -105,12 +109,12 @@ def handle(msg):
                     send_not_found_message(chat_id)
                 else:
                     bot.sendMessage(chat_id,
-                        '\n\n'.join([dep['title'] + '\n' + '/dep' + dep['id'] for dep in departments])
-                    )
-                    
-        elif(user.menu_state == MenuState.SEARCH_COURSE):
-        
-            if(msg['text'] == 'بازگشت'):
+                                    '\n\n'.join([dep['title'] + '\n' + '/dep' + dep['id'] for dep in departments])
+                                    )
+
+        elif user.menu_state == MenuState.SEARCH_COURSE:
+
+            if msg['text'] == 'بازگشت':
                 user.menu_state = MenuState.GENERAL
                 send_welcome_message(chat_id)
 
@@ -120,15 +124,16 @@ def handle(msg):
                     send_not_found_message(chat_id)
                 else:
                     bot.sendMessage(chat_id,
-                        '\n\n'.join([course_to_str(c) for c in courses])
-                    )
+                                    '\n\n'.join([course_to_str(c) for c in courses])
+                                    )
 
 
-if PROXY:
-    telepot.api.set_proxy(PROXY)
+if __name__ == '__main__':
+    if PROXY:
+        telepot.api.set_proxy(PROXY)
 
-bot = telepot.Bot(TOKEN)
-MessageLoop(bot, handle).run_as_thread()
+    bot = telepot.Bot(TOKEN)
+    MessageLoop(bot, handle).run_as_thread()
 
-while True:
-    time.sleep(30)
+    while True:
+        time.sleep(30)
